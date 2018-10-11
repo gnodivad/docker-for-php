09f574cdcb7107af1426ab9382c78186be9dbd40
David <dodwei.dev@gmail.com>
2018-10-11 19:29:34 +0800

    Using consul for the configuration management

--- custom-commands/docker/caddy.sh
+++ custom-commands/docker/caddy.sh
@@ -4,7 +4,27 @@ set -e
 
 echo "The hostname is $CADDY_HOSTNAME"
 
-confd -onetime -backend env
+function shutdown {
+    kill -s SIGTERM $CONFD_PID
+    wait $CONFD_PID
+    kill -s SIGTERM $PHP_FPM_PID
+    wait $PHP_FPM_PID
+}
+
+# confd -onetime -backend consul -node consul:8500
+until confd -onetime -backend consul -node consul:8500; do
+    echo "Waiting for the initial conf configuration..."
+    sleep 5
+done
+
+confd -interval 10 -backend consul -node consul:8500 &
+CONFD_PID=$!
+
+php-fpm &
+PHP_FPM_PID=$!
+
+trap shutdown SIGTERM SIGINT
 
 /usr/bin/caddy -validate -agree=true -conf=/etc/Caddyfile
 exec /usr/bin/caddy -agree=true -conf=/etc/Caddyfile
+
